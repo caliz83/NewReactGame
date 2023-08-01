369f2f8c4bb2057e1dd774d65b91f5ac25d1b837
caliz83 <lizg1308@gmail.com>
2023-07-31 19:49:37 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+//to open from cmd prompt:
+//npm create vite@4.1.0
+//name it
+//choose React
+//choose typescript
+//cd {file name}
+//npm install
+//npm run dev
+
+//type 'o' to open in browser
+//type 'q'  to be able to type in terminal again
+
+//code . to open in vs code
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
